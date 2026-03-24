775a697a9eb7d0319470742472cc5d50fa5edc2a
vishnu-m77 <vmugunda@uwaterloo.ca>
2026-03-24 12:17:46 -0400

    feat: Combine bool, math, exprs for analog and atomic interfaces

--- src/oqd_core/interface/math.py
+++ src/oqd_core/interface/math.py
@@ -376,7 +376,7 @@ class _MathExprIsConstant(RewriteRule):
 
     def map_MathVar(self, model):
         self.isconstant = False
-        
+    
     def map_Access(self, model):
         self.isconstant = False
 
