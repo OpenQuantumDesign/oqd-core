e03558d2f75ee72abdb853a634f022314974a734
vishnu-m77 <vmugunda@uwaterloo.ca>
2026-03-23 20:34:05 -0400

    feat: Use AtomicCircuit AST Builder

--- src/oqd_core/frontend/atomic/output.py
+++ src/oqd_core/frontend/atomic/output.py
@@ -14,7 +14,7 @@
 
 from .AtomicLexer import AtomicLexer
 from .AtomicParser import AtomicParser
-# from .AtomicCircuitAST import _AtomicASTBuilder
+from .AtomicCircuitAST import _AtomicASTBuilder
 from pathlib import Path
 import antlr4
 import typer
@@ -24,50 +24,6 @@ import typer
 ########################################################################################
 
 app = typer.Typer()
-
-def to_string_tree(root, symbolic_lexer_names, token_delimiter='"', show_token_types=True):
-    builder = []
-    _to_string_tree_traverse(root, builder, symbolic_lexer_names, token_delimiter, show_token_types)
-    return ''.join(builder)
-
-
-def _to_string_tree_traverse(tree, builder, symbolic_lexer_names, token_delimiter, show_token_types):
-    child_list_stack = [[tree]]
-
-    while len(child_list_stack) > 0:
-        child_stack = child_list_stack[-1]
-
-        if len(child_stack) == 0:
-            child_list_stack.pop()
-        else:
-            tree = child_stack.pop(0)
-            node = str(type(tree).__name__).replace('Context', '')
-            node = '{0}{1}'.format(node[0].lower(), node[1:])
-
-            indent = []
-
-            for i in range(0, len(child_list_stack) - 1):
-                indent.append('│  ' if len(child_list_stack[i]) > 0 else '   ')
-
-            token_name = ''
-            token_type = tree.getPayload().type if node.startswith('terminal') else 0
-
-            if show_token_types and token_type > -1:
-                token_name = ' ({0})'.format(symbolic_lexer_names[token_type])
-
-            builder.extend(indent)
-            builder.append('└── ' if len(child_stack) == 0 else '├── ')
-            builder.append('{0}{1}{2}{3}'.format(token_delimiter, tree.getText(), token_delimiter, token_name)
-                           if node.startswith('terminal') else node)
-            builder.append('\n')
-
-            if tree.getChildCount() > 0:
-                children = []
-                for i in range(0, tree.getChildCount()):
-                    children.append(tree.getChild(i))
-                child_list_stack.append(children)
-
-
 
 @app.command()
 def main(
@@ -81,13 +37,12 @@ def main(
     stream = antlr4.InputStream(source)
     lexer = AtomicLexer(stream)
     parser = AtomicParser(antlr4.CommonTokenStream(lexer))
-    # tree = parser.program()
-    tree = to_string_tree(parser.program(), lexer.symbolicNames)
+    tree = parser.program()
     
-    # builder = _AtomicASTBuilder()
-    # ast = builder.visit(tree)
+    builder = _AtomicASTBuilder()
+    ast = builder.visit(tree)
     # ast = analog_operator_canonicalization(ast)
-    # tree = tree.model_dump_json(indent=2)
+    tree = ast.model_dump_json(indent=2)
 
     with open(output_file, 'w') as f:
         f.write(tree)
