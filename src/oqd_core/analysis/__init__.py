eb9675819b8397687b4c6cbed25f5773d2b4703f
vishnu-m77 <vmugunda@uwaterloo.ca>
2026-06-03 14:20:45 -0400

    refactor: Move type checker and control flow graph code into analysis directory for analog interface

--- src/oqd_core/analysis/__init__.py
+++ src/oqd_core/analysis/__init__.py
@@ -1,7 +1,12 @@
-from .utils import CFGNode, SCCAnalysis
+from .utils import CFGNode
+from .analog.cfg import AnalogCFGBuilder, SCCAnalysis
+from .analog.type_checker import AnalogTypeChecker, AnalogTypeError
 
 ########################################################################################
 __all__ = [
     "CFGNode",
+    "AnalogCFGBuilder",
     "SCCAnalysis",
+    "AnalogTypeChecker",
+    "AnalogTypeError",
 ]
