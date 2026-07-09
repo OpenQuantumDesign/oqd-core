57e19ce3508ade3342bb54fe843a2b40dde6613d
vishnu-m77 <vmugunda@uwaterloo.ca>
2026-07-09 18:14:14 -0400

    feat: Add visualization for CFG

--- src/oqd_core/analysis/utils/__init__.py
+++ src/oqd_core/analysis/utils/__init__.py
@@ -1,8 +1,10 @@
 from .control_flow import Block, ControlFlowGraph, alias_types
+from .visualization import cfg_to_dot
 
 ########################################################################################
 __all__ = [
     "alias_types",
     "ControlFlowGraph",
     "Block",
+    "cfg_to_dot",
 ]
