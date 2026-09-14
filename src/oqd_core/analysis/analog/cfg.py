c265b66bafadaf311d55921843d38655e347a8f8
yhteoh <yhteoh@uwaterloo.ca>
2026-09-14 10:11:05 -0400

    [fix] start and end block empty statements

--- src/oqd_core/analysis/analog/cfg.py
+++ src/oqd_core/analysis/analog/cfg.py
@@ -39,26 +39,26 @@ class AnalogCFGBuilder(RewriteRule):
         self.preds = []
         self.edge_labels = None
         self.fallthrough_labels = {}
-        
+
     def new_node(self, preds, stmt):
         # print("edge_labels: ", self.edge_labels)
         # print("fallthrough: ", self.index, self.fallthrough_labels)
-        node = Block(register_id=self.index, stmts=[stmt], preds=preds)
+        node = Block(register_id=self.index, stmts=[stmt] if stmt else [], preds=preds)
         self.blocks[node.register_id] = node
         self.index += 1
-        
+
         explicit_labels = self.edge_labels or {}
         self.edge_labels = None
-        
+
         for pred in node.preds:
             label = explicit_labels.get(pred)
             if label is None:
                 label = self.fallthrough_labels.pop(pred, None)
                 # print(pred, label)
             self.blocks[pred].add_succ(node.register_id, label=label)
-        
+
         return node.register_id
-    
+
     def walk_stmt(self, stmt, preds, edge_labels=None):
         old = self.preds
         old_labels = self.edge_labels
@@ -68,18 +68,18 @@ class AnalogCFGBuilder(RewriteRule):
         self.preds = old
         self.edge_labels = old_labels
         return result
-    
+
     def walk_block(self, statements, preds, entry_label=None):
         edge_labels = {}
-        
+
         if entry_label:
             edge_labels = {preds[0]: entry_label}
-        
+
         for stmt in statements:
             preds = self.walk_stmt(stmt, preds, edge_labels=edge_labels)
             edge_labels = None
         return preds
-    
+
     def run(self, circuit: AnalogCircuit) -> ControlFlowGraph:
         self.index = 0
         self.blocks = {}
@@ -90,41 +90,43 @@ class AnalogCFGBuilder(RewriteRule):
         node = self.walk_stmt(circuit, [node])
         node = self.new_node(node, {})
         return ControlFlowGraph(blocks=self.blocks)
-    
+
     def map_AnalogCircuit(self, model: AnalogCircuit):
         return self.walk_block(model.statements, self.preds)
-    
+
     def map_IfElse(self, model: IfElse):
         node = self.new_node(self.preds, model.condition)
         then_branch = self.walk_block(model.then_branch, [node], entry_label="true")
         if model.else_branch:
-            else_branch = self.walk_block(model.else_branch, [node], entry_label="false")
+            else_branch = self.walk_block(
+                model.else_branch, [node], entry_label="false"
+            )
             return then_branch + else_branch
-        
+
         self.fallthrough_labels[node] = "false"
         return then_branch + [node]
-    
-    def map_While(self, model: While): 
+
+    def map_While(self, model: While):
         node = self.new_node(self.preds, model.condition)
         self.fallthrough_labels[node] = "false"
         self.loop_stack.append(node)
         body = self.walk_block(model.body, [node], entry_label="true")
         self.loop_stack.pop()
-        
+
         self.blocks[node].add_preds(body)
         for s in body:
             label = self.fallthrough_labels.pop(s, None)
             self.blocks[s].add_succ(node, label=label)
-        
+
         return self.blocks[node].exit_nodes + [node]
-    
+
     def map_Break(self, model: Break):
         if not self.loop_stack:
             raise TypeError("break statement used outside loop")
         break_node = self.new_node(self.preds, model)
         self.blocks[self.loop_stack[-1]].exit_nodes.append(break_node)
         return []
-    
+
     def map_Continue(self, model: Continue):
         if not self.loop_stack:
             raise TypeError("continue statement used outside loop")
@@ -132,7 +134,6 @@ class AnalogCFGBuilder(RewriteRule):
         self.blocks[self.loop_stack[-1]].add_pred(continue_node)
         self.blocks[continue_node].add_succ(self.loop_stack[-1])
         return []
-    
+
     def generic_map(self, model):
         return [self.new_node(self.preds, model)]
-
