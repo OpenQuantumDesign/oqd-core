53a596e22845db53fb55c36dab321a5603c7505c
yhteoh <yhteoh@uwaterloo.ca>
2026-09-14 10:34:39 -0400

    [refactor] cfg_to_dot visualization of control flow graph

--- src/oqd_core/analysis/utils/visualization.py
+++ src/oqd_core/analysis/utils/visualization.py
@@ -12,28 +12,64 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+########################################################################################
+
+
 import graphviz
+from oqd_compiler_infrastructure import RewriteRule
 
 from oqd_core.analysis.utils import ControlFlowGraph
+from oqd_core.frontend.analog.serialize import serialize_analog
+from oqd_core.frontend.atomic.serialize import serialize_atomic
+
+########################################################################################
 
 
-def cfg_to_dot(cfg: ControlFlowGraph) -> graphviz.Digraph:
-    dot = graphviz.Digraph()
+class CFGtoDot(RewriteRule):
+    def __init__(self, *, mode="analog", max_lines=5):
+        match mode:
+            case "analog":
+                self.serialize = serialize_analog
+            case "atomic":
+                self.serialize = serialize_atomic
+            case _:
+                raise ValueError("CFGtoDot only supports analog and atomic mode")
 
-    for node_id, block in sorted(cfg.blocks.items()):
-        for stmt in block.stmts:
-            stmt_label = getattr(stmt, "class__", type(stmt).__name__)
-            
-            if stmt_label == "Declaration":
-                stmt_label = f"{stmt.name} = ..."
-            if stmt_label in ("ParallelProtocol", "SerialProtocol"):
-                stmt_label = f"{stmt_label}({len(stmt.pulses)})"
-            label = (
-                f"{node_id}: {stmt_label}\\n"
-            )
-            dot.node(str(node_id), label)
+        self.max_lines = max_lines
 
-        for succ in block.succs:
-            dot.edge(str(node_id), str(succ))
+    def map_ControlFlowGraph(self, model):
+        self.dot = graphviz.Digraph()
 
-    return dot
+        for block in model.blocks.values():
+            self(block)
+
+        return self.dot
+
+    def map_Block(self, model):
+
+        if model.edge_labels:
+            label = [f"Condition: {self(model.stmts[0])}"]
+        else:
+            label = [self(stmt) for stmt in model.stmts]
+
+        if len(label) > self.max_lines and self.max_lines >= 0:
+            label = label[: self.max_lines] + ["..."]
+
+        self.dot.node(
+            str(model.register_id),
+            f"Block #{model.register_id}\n{'-' * 16}\n" + "\n".join(label),
+        )
+
+        for succ in model.succs:
+            self.dot.edge(str(model.register_id), str(succ))
+
+    def map_VisitableBaseModel(self, model):
+        return f"{self.serialize(model)}"
+
+
+def cfg_to_dot(
+    cfg: ControlFlowGraph, *, mode="analog", max_lines=5
+) -> graphviz.Digraph:
+    _cfg2dot = CFGtoDot(mode=mode, max_lines=max_lines)
+
+    return _cfg2dot(cfg)
