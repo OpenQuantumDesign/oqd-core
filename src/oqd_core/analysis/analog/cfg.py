794b10341265701ae7b19647c0e226400dccf8f5
yhteoh <yhteoh@uwaterloo.ca>
2026-09-19 13:20:45 -0400

    [feat] Implented tags for CFG and conversion from CFG back to AST

--- src/oqd_core/analysis/analog/cfg.py
+++ src/oqd_core/analysis/analog/cfg.py
@@ -15,6 +15,8 @@
 
 from __future__ import annotations
 
+from functools import reduce
+
 from oqd_compiler_infrastructure import CFG, CFGBlock, RewriteRule
 
 from oqd_core.interface.analog import (
@@ -27,9 +29,12 @@ from oqd_core.interface.analog import (
 
 
 class AnalogCFGBuilder(RewriteRule):
-    def new_node(self, preds, stmt):
+    def new_node(self, preds, stmt, tags=None):
         node = CFGBlock(
-            register_id=self.index, stmts=[stmt] if stmt else [], preds=preds
+            register_id=self.index,
+            stmts=[stmt] if stmt else [],
+            preds=preds,
+            tags=tags if tags else {},
         )
         self.blocks[node.register_id] = node
         self.index += 1
@@ -79,7 +84,11 @@ class AnalogCFGBuilder(RewriteRule):
         return CFG(blocks=self.blocks)
 
     def map_IfElse(self, model: IfElse):
-        node = self.new_node(self.preds, model.condition)
+        node = self.new_node(
+            self.preds,
+            model.condition,
+            tags={"__scf__": model.__class__.__qualname__},
+        )
         then_branch = self.walk_block(model.then_branch, [node], entry_label="true")
         if model.else_branch:
             else_branch = self.walk_block(
@@ -91,7 +100,11 @@ class AnalogCFGBuilder(RewriteRule):
         return then_branch + [node]
 
     def map_While(self, model: While):
-        node = self.new_node(self.preds, model.condition)
+        node = self.new_node(
+            self.preds,
+            model.condition,
+            tags={"__scf__": model.__class__.__qualname__},
+        )
         self.fallthrough_labels[node] = "false"
         self.loop_stack.append(node)
         body = self.walk_block(model.body, [node], entry_label="true")
@@ -121,3 +134,97 @@ class AnalogCFGBuilder(RewriteRule):
 
     def generic_map(self, model):
         return [self.new_node(self.preds, model)]
+
+
+class AnalogCFGtoAST(RewriteRule):
+    def __init__(self, pdom_result):
+        super().__init__()
+        self._pdom_result = pdom_result
+
+    @property
+    def pdom(self):
+        return self._pdom_result.out_states
+
+    @property
+    def dataflow_analysis(self):
+        return self._pdom_result.dataflow_analysis
+
+    @property
+    def lattice(self):
+        return self._pdom_result.dataflow_analysis.lattice
+
+    def _consume(self, blocks, start=0, until=None):
+        succ = start
+        statements = []
+        while succ in blocks.keys():
+            if until and succ in until:
+                break
+
+            current_block = blocks.pop(succ)
+
+            match current_block.tags.get("__scf__", None):
+                case "IfElse":
+                    ifelse_until = reduce(
+                        self.lattice.meet,
+                        [self.pdom[succ] for succ in current_block.succs],
+                    )
+
+                    reversed_edge_labels = {
+                        v: k for k, v in current_block.edge_labels.items()
+                    }
+
+                    then_block, then_succ = self._consume(
+                        blocks, start=reversed_edge_labels["true"], until=ifelse_until
+                    )
+
+                    else_block, else_succ = self._consume(
+                        blocks, start=reversed_edge_labels["false"], until=ifelse_until
+                    )
+
+                    statements.append(
+                        IfElse(
+                            condition=current_block.stmts[0],
+                            then_branch=then_block,
+                            else_branch=else_block,
+                        )
+                    )
+
+                    succ = else_succ
+
+                case "While":
+                    while_until = reduce(
+                        self.lattice.meet,
+                        [self.pdom[succ] for succ in current_block.succs],
+                    )
+
+                    reversed_edge_labels = {
+                        v: k for k, v in current_block.edge_labels.items()
+                    }
+
+                    loop_block, loop_succ = self._consume(
+                        blocks, start=reversed_edge_labels["true"], until=while_until
+                    )
+
+                    statements.append(
+                        While(condition=current_block.stmts[0], body=loop_block)
+                    )
+
+                    succ = reversed_edge_labels["false"]
+
+                case _:
+                    statements.extend(current_block.stmts)
+
+                    if not current_block.succs:
+                        break
+
+                    succ = current_block.succs[0]
+
+        return statements, succ
+
+    def map_CFG(self, model):
+        circuit = AnalogCircuit()
+
+        statements, _ = self._consume(model.blocks)
+        circuit.statements.extend(statements)
+
+        return circuit
